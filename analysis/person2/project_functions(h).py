8e6019a8b166fde8ef851f184cbfa02863c0d9fc
hennytjoa <87041118+hennytjoa@users.noreply.github.com>
2021-08-01 22:26:23 -0700

    Add files via upload

---
+++ analysis/person2/project_functions(h).py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import os
+import matplotlib.pyplot as plt
+
+def load_and_process(path):
+    df1 = (
+            pd.read_csv(path)
+            .rename(columns={"LifeExp":"LifeExpectancy"})
+            .dropna()
+            .sort_values("Country", ascending=True)
+    )
+    df2 = (
+            df1
+            .assign(color_filter=lambda x: np.where((x.Year > 1800) & (x.LifeExpectancy > 0), 1, 0))
+    )
+    
+    return df2
